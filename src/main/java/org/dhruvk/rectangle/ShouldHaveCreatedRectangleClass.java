ae3e9d3f3216d08bf7adfff328e0c46826eab285
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-07-03 23:24:59 +0530

    Fix. Explicit when java files are not found
    And not make it a default option...
    Else it can really hurt the UX when the feedback is wrong... for instance, the user has done a typo like rectangle.java and we get no java file found...

--- src/main/java/org/dhruvk/rectangle/ShouldHaveCreatedRectangleClass.java
+++ src/main/java/org/dhruvk/rectangle/ShouldHaveCreatedRectangleClass.java
@@ -18,7 +18,23 @@ public class ShouldHaveCreatedRectangleClass implements Rule {
     public Optional<String> suggestionKey() {
         if (moreThanOneFileExists()) return Optional.of("UNNECESSARY_FILES_FOUND");
         if (doesRectangleClassExist()) return Optional.empty();
-        return Optional.of("NO_JAVA_FILE_FOUND");
+        if (noJavaFileFound()) return Optional.of("NO_JAVA_FILE_FOUND");
+        return Optional.of("UNKNOWN_SCENARIO");
+    }
+
+    private boolean noJavaFileFound() { // TODO - wait for removing the duplication of the Files API... lets see enough of it to understand what will be a good abstraction
+        try {
+            long count = Files.walk(sourcePath)
+                    .filter(Files::isRegularFile)
+                    .filter(file -> file.endsWith("*.java"))
+                    .count();
+            if (count == 0) {
+                return true;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e); // This logic of traversing a path and figuring out if the file is present should likely be extracted out...
+        }
+        return false;
     }
 
     private boolean moreThanOneFileExists() {
