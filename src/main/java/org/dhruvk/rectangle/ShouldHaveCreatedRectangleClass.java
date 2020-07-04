970ebc60aab98e2257cc52556b94811583bc402d
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-07-04 13:00:54 +0530

    Refactor. Name the predicate function

--- src/main/java/org/dhruvk/rectangle/ShouldHaveCreatedRectangleClass.java
+++ src/main/java/org/dhruvk/rectangle/ShouldHaveCreatedRectangleClass.java
@@ -1,6 +1,5 @@
 package org.dhruvk.rectangle;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,7 +12,6 @@ import static org.apache.commons.io.FilenameUtils.getExtension;
 public class ShouldHaveCreatedRectangleClass implements Rule {
 
     private final Path absoluteSourcePath;
-    private static final Pattern fileStartsWithLowerCase = Pattern.compile("[a-z]+.*");
 
     public ShouldHaveCreatedRectangleClass(Path sourceAbsolutePath) {
         assert sourceAbsolutePath.startsWith("/") : "Expected absolute path, but looks like you passed a relative path -> " + sourceAbsolutePath;
@@ -35,10 +33,17 @@ public class ShouldHaveCreatedRectangleClass implements Rule {
             return Files.walk(absoluteSourcePath)
                     .filter(Files::isRegularFile)
                     .filter(isJavaFile())
-                    .anyMatch(file -> fileStartsWithLowerCase.matcher(file.getFileName().toString()).matches());
+                    .anyMatch(fileStartsWithALowerCase());
         } catch (IOException e) {
             throw new RuntimeException(e); // This logic of traversing a path and figuring out if the file is present should likely be extracted out...
         }
+    }
+
+    private Predicate<Path> fileStartsWithALowerCase() {
+        return file -> {
+            String lowerCaseRegex = "[a-z]+.*";
+            return Pattern.compile(lowerCaseRegex).matcher(file.getFileName().toString()).matches();
+        };
     }
 
     private boolean noJavaFileFound() { // TODO - wait for removing the duplication of the Files API... lets see enough of it to understand what will be a good abstraction
