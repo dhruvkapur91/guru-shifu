4e420b1cca40f77af2de266e148e057eb00c3979
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-07-04 11:42:50 +0530

    Refactor. Test, generate directories instead of hardcoding them in resources. Earlier it felt like hardcoding the directories in resources was a good idea but it quickly became unmaintainable... while I still see value in having all codes in some directory structure for quick inspection, but I feel that should be generaable by some gradle task and not necessarily be in the version control....

--- src/test/java/org/dhruvk/rectangle/ShouldHaveCreatedRectangleClassTest.java
+++ src/test/java/org/dhruvk/rectangle/ShouldHaveCreatedRectangleClassTest.java
@@ -2,52 +2,58 @@ package org.dhruvk.rectangle;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
+import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static java.nio.file.Paths.get;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class ShouldHaveCreatedRectangleClassTest {
-    // Todo not sure if these should be directories in source code or generated per test and then cleared up?
-    // I'm inclining to create them on fly now, with a feature of persistance/inspection if required... there is a lot of duplication in names and it'll cause difficulty in refactoring if required
-    private static final Path directoryWithNoJavaFiles = get("src/test/resources/org/dhruvk/rectangle/directory_with_no_java_files").toAbsolutePath();
-    private static final Path directoryWithOnlyRectangleFile = get("src/test/resources/org/dhruvk/rectangle/directory_with_only_rectangle_file").toAbsolutePath();
-    private static final Path directoryWithOnlyRectangleFileWithoutFollowingConventions = get("src/test/resources/org/dhruvk/rectangle/directory_with_only_rectangle_file_without_following_conventions").toAbsolutePath();
-    private static final Path directoryWithRectanglePlusUnnecessaryFiles = get("src/test/resources/org/dhruvk/rectangle/directory_with_rectangle_plus_unnecessary_files").toAbsolutePath();
-    private static final Path directoryWithRectangleFileWithTypos = get("src/test/resources/org/dhruvk/rectangle/directory_with_only_rectangle_file_with_some_typos").toAbsolutePath();
 
-//     TODO - unable to add test for empty directory as git won't add it?
+    //TODO - unable to add test for empty directory as git won't add it?
 
     @Test
-    void shouldGiveFeedbackForDirectoryContainingNoJavaFiles() {
-        assertThat(findFeedbackFor(directoryWithNoJavaFiles), is(Optional.of("NO_JAVA_FILE_FOUND"))); // TODO - is it possible to get this as a key from resource bundle to prevent hardcoding...?
+    void shouldGiveFeedbackForDirectoryContainingNoJavaFiles(@TempDir File someFile) throws Exception {
+        Path path = setupDirectoryStructure(someFile, "org/dhruvk/rectangle", "blah.txt");
+        assertThat(findFeedbackFor(path), is(Optional.of("NO_JAVA_FILE_FOUND"))); // TODO - is it possible to get this as a key from resource bundle to prevent hardcoding...?
     }
 
     @Test
-    void shouldGiveNoFeedbackForDirectoryContainingRectangleClassFile() {
-        assertThat(findFeedbackFor(directoryWithOnlyRectangleFile), is(Optional.empty()));
+    void shouldGiveNoFeedbackForDirectoryContainingRectangleClassFile(@TempDir File someFile) throws Exception {
+        Path path = setupDirectoryStructure(someFile, "org/dhruvk/rectangle", "Rectangle.java");
+        assertThat(findFeedbackFor(path), is(Optional.empty()));
     }
 
     @Test
-    void shouldGiveFeedbackForDirectoryContainingUnnecessaryClassFile() {
+    void shouldGiveFeedbackForDirectoryContainingUnnecessaryClassFile(@TempDir File someFile) throws Exception {
+        Path path = setupDirectoryStructure(someFile, "org/dhruvk/rectangle", "Rectangle.java");
+        setupDirectoryStructure(someFile, "org/dhruvk/rectangle", "Blah.java");
         Optional<String> expected = Optional.of("UNNECESSARY_FILES_FOUND");
-        assertThat(findFeedbackFor(directoryWithRectanglePlusUnnecessaryFiles), is(expected));
+        assertThat(findFeedbackFor(path), is(expected));
     }
 
     @Test
-    void shouldGiveFeedbackForIfNameOfTheClassDoesNotFollowJavaConventions() {
+    void shouldGiveFeedbackForIfNameOfTheClassDoesNotFollowJavaConventions(@TempDir File someFile) throws Exception {
+        Path path = setupDirectoryStructure(someFile, "org/dhruvk/rectangle", "rectangle.java");
         Optional<String> expected = Optional.of("JAVA_FILE_NAMING_CONVENTIONS_NOT_FOLLOWED");
-        assertThat(findFeedbackFor(directoryWithOnlyRectangleFileWithoutFollowingConventions), is(expected));
+        assertThat(findFeedbackFor(path), is(expected));
     }
 
     @Test
-    void shouldReportUnknownScenarioWhenSeeingATypoForNow() {
-        assertThat(findFeedbackFor(directoryWithRectangleFileWithTypos), is(Optional.of("UNKNOWN_SCENARIO")));
+    void shouldReportUnknownScenarioWhenSeeingATypoForNow(@TempDir File someFile) throws Exception {
+        Path path = setupDirectoryStructure(someFile, "org/dhruvk/rectangle", "Recangle.java");
+        assertThat(findFeedbackFor(path), is(Optional.of("UNKNOWN_SCENARIO")));
     }
 
     @Test
@@ -58,5 +64,16 @@ class ShouldHaveCreatedRectangleClassTest {
 
     Optional<String> findFeedbackFor(Path path) {
         return new ShouldHaveCreatedRectangleClass(path).suggestionKey();
+    }
+
+    // TODO - primitive obsessions with the strings, should see if there are better abstractions
+    // TODO - irritating code...
+    Path setupDirectoryStructure(File someTemporaryFile, String packageStructure, String fileNameWithExtension) throws Exception {
+        File file = new File(someTemporaryFile.getAbsolutePath() + "/" + packageStructure);
+        file.mkdirs();
+        file.toPath().resolve(fileNameWithExtension);
+        Files.write(file.toPath().resolve(fileNameWithExtension), List.of(""));
+        assertTrue(Files.exists(Paths.get(file.getAbsolutePath() + "/" + fileNameWithExtension)));
+        return file.toPath();
     }
 }
