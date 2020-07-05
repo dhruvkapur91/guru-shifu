3a16ce0296d3d1127c3f039b7c82a010e699c17f
Dhruv Kapur <dhruvkapur91@gmail.com>
2020-07-05 15:30:33 +0530

    Refactor. Start replace if with case statements - 3

--- src/main/scala/org/dhruvk/rectangle/RectangleCodeMetrics.scala
+++ src/main/scala/org/dhruvk/rectangle/RectangleCodeMetrics.scala
@@ -99,14 +99,11 @@ case class RectangleCodeMetrics() {
       case (_,false,_,_,_,_) => throw new RuntimeException("Did not find a method that can be called")
       case (_,_,2,0,_,_) => return Seq(s"new ${getClassName.get}(${rectangle.length},${rectangle.breath}).${getCallableMethod.get}()")
       case (_,_,0,2,false,false) => return Seq(s"new ${getClassName.get}().${getCallableMethod.get}(${rectangle.length},${rectangle.breath})")
+      case (_,_,0,2,false,true) => return Seq(s"${getClassName.get}.${getCallableMethod.get}(${rectangle.length},${rectangle.breath})")
       case _ => ""
 
     }
 
-
-
-    //    // Assuming no constuctor, there is a procedural callable method and its static
-    if (!hasConstructor && getCallableMethod.isDefined && isCallableMethodStatic && numberOfCallableMethodParameters == 2) return Seq(s"${getClassName.get}.${getCallableMethod.get}(${rectangle.length},${rectangle.breath})")
     //
 
 
